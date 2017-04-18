140768ed837e5b48ba9597313c016b6234730663
finpickford <u1362372@unimail.hud.ac.uk>
2017-04-18 14:44:08 +0100

    Added a model for the user in the domain folder. Added a model so that a user can be set in the controller using a set method. Which is then output in an index file.

--- src/main/java/com/wristwatch/controller/HomeController.java
+++ src/main/java/com/wristwatch/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.wristwatch.controller;
 
+import com.wristwatch.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +15,15 @@ public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
 //    @ResponseBody
-    public String index() {
+    public String index(Model model) {
+
+        User user = new User();
+
+        user.setFirstname("Fin");
+        user.setLastname("Pickford");
+        user.setPassword("test");
+
+        model.addAttribute( "user", user);
         return "index";
     }
 }
---
+++ src/main/java/com/wristwatch/domain/User.java
@@ -0,0 +1,36 @@
+package com.wristwatch.domain;
+
+/**
+ * Created by web on 18/04/17.
+ */
+public class User {
+
+    String firstname;
+    String lastname;
+    String password;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
