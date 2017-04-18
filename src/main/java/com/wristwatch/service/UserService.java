53212958b0369b58dc1a1f04ebe2dcfabc3499f8
finpickford <u1362372@unimail.hud.ac.uk>
2017-04-18 15:54:53 +0100

    Added a user repo. Added a user repo, so that when someone registers, it adds their user details into a repo database using jpa h2.

---
+++ src/main/java/com/wristwatch/service/UserService.java
@@ -0,0 +1,22 @@
+package com.wristwatch.service;
+
+import com.wristwatch.domain.User;
+import com.wristwatch.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by web on 18/04/17.
+ */
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User save(User u) {
+        return userRepository.save(u);
+    }
+
+}
