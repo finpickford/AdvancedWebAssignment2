f6681e41b634b01109c624e5479634c2bced716c
finpickford <u1362372@unimail.hud.ac.uk>
2017-04-24 16:43:37 +0100

    Adding a function to be able to search for a brand. Added methods into the repo, service and controller to output a brand that the user searches for.

--- src/main/java/com/wristwatch/domain/WatchbrandRepository.java
+++ src/main/java/com/wristwatch/domain/WatchbrandRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
  * Created by web on 24/04/17.
  */
 public interface WatchbrandRepository extends JpaRepository<Watchbrand, Long> {
-    @Query("SELECT watchbrand from Watchbrand watchbrand WHERE watchbrand.brandname LIKE ?1")
+
+    @Query("SELECT watchbrand from Watchbrand watchbrand WHERE watchbrand.brandname LIKE %?1%")
     List<Watchbrand> searchWatchbrand(String brandname);
 }
